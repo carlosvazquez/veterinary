2ce4039bf9e456e598eb59ed1540152f3757601f
MARTINEZ JUAREZ ALFONSO <alfonso.martinezj@ine.mx>
2020-12-21 15:06:31 -0600

    make it better

---
+++ app.js
@@ -0,0 +1,36 @@
+/* Alumno Alfonso Martínez Juárez UNIR */
+/* index.js es el encargado de arrancar la aplicación, la base de satos y servidor, así como otras configuraciones básicas*/
+
+const express = require('express')
+const app = express();
+const port = 3000;
+app.use(express.static(__dirname + "/public"));
+
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/', (req, res) => {
+  res.render("index", {titulo: ""})
+})
+
+app.get('/servicios', (req, res) => {
+    res.render("servicios", { tituloServicios: "Este es un mensaje dinámico de servicios"})
+  })
+
+
+
+app.get('/nosotros', (req, res) => {
+    res.render("nosotros", {tituloNosotros: "Este es un mensaje dinámico de nosotros"})
+  })
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Título del sitio web"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor a su servicio en el puerto`, port)
+})
---
+++ frutas.js
@@ -0,0 +1,10 @@
+/* Alumno Alfonso Martínez Juárez UNIR */
+/* index.js es el encargado de arrancar la aplicación, la base de satos y servidor, así como otras configuraciones básicas*/
+
+const frutas = ['manzana', 'platano', 'pera', 'fresa',];
+const dinero = 1000;
+
+module.exports = {
+    frutas: frutas,
+    dinero: dinero
+}
