3fe9c622eb9db7f981535d4e34d5aef553de4ac7
MARTINEZ JUAREZ ALFONSO <alfonso.martinezj@ine.mx>
2020-12-24 14:09:32 -0600

    make it better

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.use(express.static(__dirname + "/public"));
 
 //Conexión a base de datos
