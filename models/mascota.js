6957386f495c9041457a89cf79e02e9a6267dc70
Charles <carlosvazquezmurillo@gmail.com>
2020-12-27 23:40:51 -0600

    Add veterinaria

--- models/mascota.js
+++ models/mascota.js
@@ -7,6 +7,6 @@ const mascotaSchema = new Schema({
 })
 
 // Crear el modelo
-const Mascota = mongoose.model('mascotas', mascotaSchema);
+const Mascota = mongoose.model('veterinaria.mascotas', mascotaSchema);
 
 module.exports = Mascota;
